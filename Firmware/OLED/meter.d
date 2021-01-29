6284686a34378e5bf57590a011d4fc22acd59e9e
Jacob <jake.arsenault@hotmail.ca>
2021-01-29 13:13:45 -0500

    added firmware code for OLED and BME680

---
+++ Firmware/OLED/meter.d
@@ -0,0 +1,23 @@
+meter.o: meter.c /opt/gcc-arm/arm-none-eabi/include/math.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/reent.h \
+ /opt/gcc-arm/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm/arm-none-eabi/include/newlib.h \
+ /opt/gcc-arm/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/config.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stddef.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/lock.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/cdefs.h \
+ /opt/gcc-arm/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm/arm-none-eabi/include/string.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/string.h ugui.h ugui_config.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_stdint.h meter.h \
+ /home/username/stm32/stm32f103c8t6//rtos/libwwg/include/miniprintf.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdarg.h oled.h
---
+++ Firmware/OLED/ugui.d
@@ -0,0 +1,8 @@
+ugui.o: ugui.c ugui.h ugui_config.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_stdint.h
