7b7956a22689b1dcffaf0c4d249b30ec23fbdb3e
Jake Arsenault <jake.arsenault@hotmail.ca>
2021-02-26 13:36:04 -0500

    OLED display now pulls data from firebase to be displayed

--- srcs/database/database.py
+++ srcs/database/database.py
@@ -4,7 +4,7 @@ config = {
         "apiKey": "19b17a41c94eeece3ba3a29a0095cf52f89392ed",
         "authDomain": "https://JDSecurity.firebaseio.com",
 	"databaseURL": "https://jdsecuritysolutions-5726a.firebaseio.com/",
-	"storageBucket": "gs://jdsecuritysolutions-5726a.appspot.com/",
+	"storageBucket": "jdsecuritysolutions-5726a.appspot.com",
 }
 
 
@@ -12,22 +12,41 @@ config = {
 firebase = pyrebase.initialize_app(config)
 
 # initialisatiing Database
-db = firebase.database()
+#db = firebase.database()
+
+username = "jacob"
+room = "BedRoom"
+
+#storagepath = username+"/"+room+"/test.txt"
+
+storage = firebase.storage()
+#fbupload = storage.child(storagepath).put("test.txt")
+
 
 # selecting the column in the database
-db.child("users").child("Morty")
+#db.child("Usernames")
 
 # How to save a data
-data = {"name": "Mortimer 'Morty' Smith"}
-db.child("Rick").push(data)
+#data = {"name": "Mortimer 'Morty' Smith",
+#        "email": "wuubalubadubdub",
+#        "password": "mrmeeseeks",
+#        "username": "Morty"}
+
+#db.child("Morty").push(data)
 
 # How to update a existing data
-#db.child("users").child("Morty").update({"name": "Mortiest Morty"})
+#db.child("Morty").update(data)
 
 # Removing existing data
 #db.child("users").child("Morty").remove()
 
-# reterving the data
-#users = db.child("users").get()
-#print(users.val()) #  retervies  this data => {"Morty": {"name": "Mortimer 'Morty' Smith"}, "Rick": {"name": "Rick Sanchez"}}
+# reterving all the data
+storage.child("jacob/BedRoom/test.txt").put("test.txt")
 
+
+
+#retieve data by name
+#user_by_name = db.child("Usernames").order_by_child("name").get()
+
+
+#print(user_by_name.val())
