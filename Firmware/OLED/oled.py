a0b595d0548572caafd54e497248b7994c3f6c6f
Jake Arsenault <jake.arsenault@hotmail.ca>
2021-04-08 19:37:23 -0400

    changed oled firmware to account for new oled module, hid api keys

--- Firmware/OLED/oled.py
+++ Firmware/OLED/oled.py
@@ -1,86 +1,49 @@
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.oled.device import ssd1309
+
 import os.path
 import signal
 import sys
 import time
 import pyrebase
-
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
 import subprocess
 
 #Pyrebase Config to be moved to env var
 config = {
-        "apiKey": "19b17a41c94eeece3ba3a29a0095cf52f89392ed",
-        "authDomain": "https://JDSecurity.firebaseio.com",
-	"databaseURL": "https://jdsecuritysolutions-5726a.firebaseio.com/",
-	"storageBucket": "jdsecuritysolutions-5726a.appspot.com",
+        "apiKey": "",
+        "authDomain": "",
+	"databaseURL": "",
+	"storageBucket": "",
 }
 
 
 # Manage Keyboard Interupt and close program while clearing screen.
 def sigint_handler(signal, frame):
     print('Closing Program due to Interupt')
-    disp.clear()
-    disp.display()
     sys.exit(0)
 signal.signal(signal.SIGINT, sigint_handler)
 
 
-# Raspberry Pi pin configuration:
-RST = 25
-DC = 24
-SPI_PORT = 0
-SPI_DEVICE = 0
-
-# 128x64 display with hardware SPI:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-
-# Initialize library.
-disp.begin()
+serial = spi(device=0, port=0)
+device = ssd1309(serial)
 
 # Initialize Pyrebase
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
 
-# Download temperature data
-storage.child("jacob/BedRoom/temp.txt").download("dataValues.txt")
 
 
-# Clear display.
-disp.clear()
-disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
-
-
-# Load default font.
-font = ImageFont.load_default()
+i = 1
+c = 0
+hasDrawn = False
 
 while True:
-
+    # Download temperature data
+    storage.child("jacob/BedRoom/sensorReadings.txt").download("sensorReadings.txt")
+    
     # File Containing Sensor Data
-    fileName = "dataValues.txt"
+    fileName = "sensorReadings.txt"
 
     # Open file reading in lines
     if not os.path.isfile(fileName):
@@ -88,23 +51,19 @@ while True:
     else:
         with open(fileName) as f:
             content = f.readlines()
-    # Close file once lines have been read in
+    #Close file once lines have been read in
     f.close()
-    
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-    
-    # 
-    i = 0
+
+    hasDrawn = False
+    with canvas(device) as draw:
+            print("Data Entry:",i)
+            for line in content:
+                if c>=((i-1)*6) and c<(i*6):
+                    print("Drawing Line:",c)
+                    hasDrawn = True
+                    draw.text((1, (c%6)*10), line, fill="white")
+                c+=1
+            if (hasDrawn == True):
+                i += 1
+    time.sleep(5)
     c = 0
-    for line in content:
-        if c<5:
-            c+=1
-            draw.text((x, top+int(i)), str(line), font=font, fill=255)
-            i = i + 8
-
-
-    # Display image.
-    disp.image(image)
-    disp.display()
-    time.sleep(.1)
