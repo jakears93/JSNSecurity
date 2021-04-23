703856a0b8cff482f50a9f92c6073bd6dfb810ba
satudalvadi <n01333116@humbermail.ca>
2021-04-22 22:32:06 -0400

    Updated sdCard.py

--- Firmware/SDCard/sdCard.py
+++ Firmware/SDCard/sdCard.py
@@ -4,6 +4,8 @@ import sys
 import time
 import pyrebase
 import subprocess
+import serial
+from serial import Serial
 
 #Import Pyrebase Config from file
 config = {}
@@ -27,7 +29,16 @@ storage = firebase.storage()
 while True:
     # Download temperature data
     storage.child("jacob/BedRoom/sensorReadings.txt").download("sensorReadings.txt")
-    time.sleep(30)
+    ser = serial.Serial("/dev/ttyACM0",9600)
+  
+    ser.write('x'.encode())
+
+  #f = os.open("/dev/ttyACM0",os.O_RDWR)
+  #f.write(open("sensorReadings.txt","rb").read())
+  #f.write(open("sensorReadings.txt").read())
+  #f.close()
+    print('backup complete') 
+    time.sleep(5)
 	
 # Manage Keyboard Interupt and close program while clearing screen.
 def sigint_handler(signal, frame):
